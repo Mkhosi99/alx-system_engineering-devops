361edf0fdead712748071a48b854a5af08d4cd71
Mkhosi99 <mkhosishabalala2@gmail.com>
2024-05-08 23:33:01 +0200

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""function that queries the reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit."""
-
+"""Script that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """function that queries the reddit API and prints the
-        titles of the first 10 hot posts listed for
-        a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
+    """Prints the titles of the first 10 hot posts listed for a given
+    subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {"User-Agent": "My-User-Agent"}
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
+    if response.status_code >= 300:
         print("None")
+    else:
+        [print(child.get("data").get("title"))
+         for child in response.json().get("data").get("children")]
